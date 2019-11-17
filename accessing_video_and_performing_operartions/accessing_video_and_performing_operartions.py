fad9a55c64658700028d3da605413f7e431826a5
iad7kor <iad7kor@ECGCN1069655L>
2019-11-17 13:04:36 +0530

    Add project files.

---
+++ accessing_video_and_performing_operartions/accessing_video_and_performing_operartions.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+def access_camera():
+    
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+    """
+    For example, I can check the frame width and height by cap.get(3) and cap.get(4).
+    It gives me 640x480 by default. But I want to modify it to 320x240.
+    Just use ret = cap.set(3,320) and ret = cap.set(4,240).
+   """
+
+def save_video():
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            frame = cv2.flip(frame,0)
+
+            # write the flipped frame
+            out.write(frame)
+
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+save_video()
+
